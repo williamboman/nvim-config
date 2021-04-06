17c48acd76fc7b6efb313230d5e46294e378929f
William Boman <william@redwill.se>
2021-04-06 21:24:48 +0200

    youre fireeeeeeeeeeed

--- lua/wb/keymaps.lua
+++ lua/wb/keymaps.lua
@@ -7,8 +7,6 @@ vim.api.nvim_set_keymap('n',  '<S-Tab>',           '<cmd>cprev<CR>',      {norem
 vim.api.nvim_set_keymap('n',  '<leader><leader>',  '<cmd>up<CR>',         {noremap=true})
 vim.api.nvim_set_keymap('n',  '<leader>.',         '<cmd>q<CR>',          {noremap=true})
 vim.api.nvim_set_keymap('n',  '<C-w>e',            '<cmd>tab split<CR>',  {noremap=true})
-vim.api.nvim_set_keymap('v',  '<',                 '<gv',                 {})
-vim.api.nvim_set_keymap('v',  '>',                 '>gv',                 {})
 
 --i want to be able to leave terminal yes pls
 vim.api.nvim_set_keymap('t',  '<Esc>',             '<C-\\><C-n>',         {noremap=true})
