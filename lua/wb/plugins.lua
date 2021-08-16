13b91b4345f9e748ffd5bd9101d1559a5250503e
William Boman <william@redwill.se>
2021-08-16 02:18:46 +0200

    plugins re-enable indent-blankline.nvim

--- lua/wb/plugins.lua
+++ lua/wb/plugins.lua
@@ -173,16 +173,16 @@ return require("packer").startup(function(use, use_rocks)
         },
         "christianchiarulli/nvcode-color-schemes.vim",
         "kyazdani42/nvim-web-devicons",
-        -- { 'lukas-reineke/indent-blankline.nvim',
-        --     branch = 'lua',
-        --     setup = function ()
-        --         vim.g.indent_blankline_use_treesitter = true
-        --         vim.g.indent_blankline_buftype_exclude = {'terminal', 'nofile', }
-        --         vim.g.indent_blankline_filetype_exclude = {'help', 'packer', }
-        --         vim.g.indent_blankline_char = '▏'
-        --         vim.cmd([[set colorcolumn=99999]]) -- fix indentline for now
-        --     end,
-        -- },
+        {
+            "lukas-reineke/indent-blankline.nvim",
+            setup = function()
+                vim.g.indent_blankline_use_treesitter = true
+                vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+                vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
+                vim.g.indent_blankline_char = "▏"
+                vim.cmd [[set colorcolumn=99999]]
+            end,
+        },
         {
             "norcalli/nvim-colorizer.lua",
             config = function()
@@ -218,7 +218,7 @@ return require("packer").startup(function(use, use_rocks)
                 "neovim/nvim-lspconfig",
             },
         },
-        { "mfussenegger/nvim-jdtls", ft = { "java" } },
+        -- { "mfussenegger/nvim-jdtls", ft = { "java" } },
         {
             "onsails/lspkind-nvim",
             config = function()
