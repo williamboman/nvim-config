7772656f65c4a0a220754e69bc8bad3db3342cf2
William Boman <william@redwill.se>
2021-11-09 21:04:53 +0100

    jdtls: map language/status to $/progress

--- lua/wb/lsp/init.lua
+++ lua/wb/lsp/init.lua
@@ -92,6 +92,76 @@ function M.setup()
                     },
                 })
             end,
+            ["jdtls"] = function()
+                -- Non-standard notification that can be used to display progress
+                local function create_language_status_handler()
+                    ---@param result {type:"Starting"|"Started"|"ServiceReady", message:string}
+                    ---@type table<string, boolean>
+                    local tokens = {}
+                    ---@type table<string, boolean>
+                    local ready_projects = {}
+                    return function(_, result, ctx)
+                        local cwd = vim.loop.cwd()
+                        if ready_projects[cwd] then
+                            return
+                        end
+                        local token = tokens[cwd] or vim.tbl_count(tokens)
+                        if result.type == "Starting" and not tokens[cwd] then
+                            tokens[cwd] = token
+                            vim.lsp.handlers["$/progress"](nil, {
+                                token = token,
+                                value = {
+                                    kind = "begin",
+                                    title = "jdtls",
+                                    message = result.message,
+                                    percentage = 0,
+                                },
+                            }, ctx)
+                        elseif result.type == "Starting" then
+                            local _, percentage_index = string.find(result.message, "^%d%d?%d?")
+                            local percentage = 0
+                            local message = result.message
+                            if percentage_index then
+                                percentage = tonumber(string.sub(result.message, 1, percentage_index))
+                                message = string.sub(result.message, percentage_index + 3)
+                            end
+
+                            vim.lsp.handlers["$/progress"](nil, {
+                                token = token,
+                                value = {
+                                    kind = "report",
+                                    message = message,
+                                    percentage = percentage,
+                                },
+                            }, ctx)
+                        elseif result.type == "Started" then
+                            vim.lsp.handlers["$/progress"](nil, {
+                                token = token,
+                                value = {
+                                    kind = "report",
+                                    message = result.message,
+                                    percentage = 100,
+                                },
+                            }, ctx)
+                        elseif result.type == "ServiceReady" then
+                            ready_projects[cwd] = true
+                            vim.lsp.handlers["$/progress"](nil, {
+                                token = token,
+                                value = {
+                                    kind = "end",
+                                    message = result.message,
+                                },
+                            }, ctx)
+                        end
+                    end
+                end
+
+                return vim.tbl_deep_extend("force", default_opts, {
+                    handlers = {
+                        ["language/status"] = create_language_status_handler(),
+                    },
+                })
+            end,
         }
 
         server:setup(
