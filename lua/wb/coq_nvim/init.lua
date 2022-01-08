430aa86d0159112aaa4c62400457bc0f915a1882
William Boman <william@redwill.se>
2022-01-08 15:29:51 +0100

    coq: add copilot source

--- lua/wb/coq_nvim/init.lua
+++ lua/wb/coq_nvim/init.lua
@@ -1,13 +1,8 @@
 local M = {}
 
 function M.setup()
-    -- these are same as default, but we define them ourselves for better autopairs interop
-    vim.api.nvim_set_keymap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-    vim.api.nvim_set_keymap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-    vim.api.nvim_set_keymap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-    vim.api.nvim_set_keymap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
-
     local sources = {
+        { src = "copilot", short_name = "COP", accept_key = "<c-f>" },
         { src = "bc", short_name = "MATH", precision = 6 },
         { src = "repl", unsafe = { "rm", "sudo", "mv", "cp" } },
     }
