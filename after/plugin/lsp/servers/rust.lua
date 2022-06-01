b633124a04543c1863ee43e2d82bd3f17233ef3b
William Boman <william@redwill.se>
2022-06-01 17:39:23 +0200

    rust improvements

--- after/plugin/lsp/servers/rust.lua
+++ after/plugin/lsp/servers/rust.lua
@@ -3,4 +3,18 @@ if not ok then
     return
 end
 
-rust_tools.setup {}
+rust_tools.setup {
+    tools = {
+        executor = require("rust-tools/executors").toggleterm,
+        hover_actions = { border = "solid" },
+    },
+}
+
+local inlay_fix
+inlay_fix = vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
+    pattern = "*.rs",
+    callback = function()
+        vim.cmd [[RustSetInlayHints]]
+        vim.api.nvim_del_autocmd(inlay_fix)
+    end,
+})
