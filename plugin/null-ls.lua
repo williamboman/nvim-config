378cae4a0b7343990b298424ed66282996de1449
William Boman <william@redwill.se>
2022-08-11 01:27:23 +0200

    null-ls: enable more sources

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -3,15 +3,23 @@ if not ok then
     return
 end
 
+local editorconfig_checker = null_ls.builtins.diagnostics.editorconfig_checker
+editorconfig_checker._opts.command = "editorconfig-checker"
+
 null_ls.setup {
     sources = {
         null_ls.builtins.diagnostics.actionlint,
         null_ls.builtins.diagnostics.codespell,
+        editorconfig_checker,
+        null_ls.builtins.diagnostics.gitlint,
         null_ls.builtins.diagnostics.misspell,
         null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.yamllint,
+        null_ls.builtins.formatting.jq,
         null_ls.builtins.formatting.ktlint,
         null_ls.builtins.formatting.markdownlint,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.shellharden,
         null_ls.builtins.formatting.stylua,
     },
     on_attach = require "wb.lsp.on-attach",
