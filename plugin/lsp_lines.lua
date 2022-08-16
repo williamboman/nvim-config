834aa7664697d46204148bd4a09c7b1e118c57c9
William Boman <william@redwill.se>
2022-08-16 12:52:17 +0200

    improve diagnostic toggling

--- plugin/lsp_lines.lua
+++ plugin/lsp_lines.lua
@@ -9,15 +9,43 @@ lsp_lines.setup()
 vim.diagnostic.config {
     virtual_lines = false,
     signs = false,
+    severity_sort = true,
     float = {
         header = false,
         source = "always",
     },
 }
 
-vim.keymap.set(
-    "n",
-    "<M-d>",
-    require("lsp_lines").toggle,
-    { desc = "Toggle lsp_lines" }
-)
+local Mode = {
+    "Text",
+    "Lines",
+    "None",
+}
+
+local current_mode = 1
+
+vim.keymap.set("n", "<M-d>", function()
+    current_mode = (current_mode % #Mode) + 1
+    if Mode[current_mode] == "Text" then
+        vim.diagnostic.config {
+            signs = false,
+            underline = true,
+            virtual_text = true,
+            virtual_lines = false,
+        }
+    elseif Mode[current_mode] == "Lines" then
+        vim.diagnostic.config {
+            signs = false,
+            underline = true,
+            virtual_text = false,
+            virtual_lines = true,
+        }
+    elseif Mode[current_mode] == "None" then
+        vim.diagnostic.config {
+            signs = true,
+            underline = false,
+            virtual_text = false,
+            virtual_lines = false,
+        }
+    end
+end, { desc = "Toggle lsp_lines" })
