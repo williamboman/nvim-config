2b5578859e7f1d0ad01ed3bbf67ff77fda1549e7
William Boman <william@redwill.se>
2022-09-04 18:20:03 +0200

    dressing: disable input

--- plugin/dressing.lua
+++ plugin/dressing.lua
@@ -5,6 +5,7 @@ end
 
 dressing.setup {
     input = {
+        enabled = false, -- native vim input is pretty nice for many reasons
         winblend = 10,
         winhighlight = "Normal:DressingInputNormalFloat,NormalFloat:DressingInputNormalFloat,FloatBorder:DressingInputFloatBorder",
         border = "single",
