fac33776d0f97105ac3ab1b3cefa62471e96e337
William Boman <william@redwill.se>
2023-01-29 02:01:55 +0000

    fix tint.nvim settings

--- after/plugin/tint.lua
+++ after/plugin/tint.lua
@@ -6,8 +6,13 @@ end
 tint.setup {
     tint = -20,
     saturation = 0.6,
-    ignore = { "WinBar.*", "WinSeparator", "IndentBlankline.*", "SignColumn", "EndOfBuffer" },
-    ignorefunc = function(winid)
+    highlight_ignore_patterns = { "WinBar.*", "WinSeparator", "IndentBlankline.*", "SignColumn", "EndOfBuffer" },
+    window_ignore_function = function(winid)
+        -- Don't tint floating windows.
+        if vim.api.nvim_win_get_config(winid).relative ~= "" then
+            return true
+        end
+
         local bufnr = vim.api.nvim_win_get_buf(winid)
         -- Only tint normal buffers.
         return vim.api.nvim_buf_get_option(bufnr, "buftype") ~= ""
