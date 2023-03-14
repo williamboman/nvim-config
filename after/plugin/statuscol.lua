4e33d7525b02f645ed8fa0a361244c81d02c9c96
William Boman <william@redwill.se>
2023-03-14 12:09:03 +0100

    fix statuscol settings

--- after/plugin/statuscol.lua
+++ after/plugin/statuscol.lua
@@ -3,9 +3,19 @@ if not ok then
     return
 end
 
+local builtin = require "statuscol.builtin"
+
 statuscol.setup {
     separator = "│",
     foldfunc = "builtin",
     setopt = true,
-    order = "NSFs",
+    segments = {
+        {
+            text = { " ", builtin.lnumfunc, " " },
+            condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+        },
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        { text = { "%s" }, click = "v:lua.ScSa" },
+    },
 }
