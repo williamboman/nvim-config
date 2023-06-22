fdf43d01f9492791ddd4451a6052773e289491b7
William Boman <william@redwill.se>
2023-06-22 02:59:01 +0200

    fix gitsigns

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -4,26 +4,36 @@ if not ok then
 end
 
 gitsigns.setup {
-    keymaps = {},
     current_line_blame = false,
     current_line_blame_opts = {
         delay = vim.o.updatetime,
     },
+    on_attach = function(bufnr)
+        vim.keymap.set("n", "]c", function()
+            if vim.wo.diff then
+                return "]c"
+            end
+            gitsigns.next_hunk()
+        end, { buffer = bufnr })
+        vim.keymap.set("n", "[c", function()
+            if vim.wo.diff then
+                return "[c"
+            end
+            gitsigns.prev_hunk()
+        end, { buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>ga", gitsigns.stage_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>gu", gitsigns.undo_stage_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>gq", function()
+            gitsigns.setqflist "all"
+        end, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>g.q", function()
+            gitsigns.setqflist(0)
+        end, { buffer = bufnr })
+
+        vim.keymap.set("o", "ah", gitsigns.select_hunk, { buffer = bufnr })
+        vim.keymap.set("v", "ah", gitsigns.select_hunk, { buffer = bufnr })
+    end,
 }
-
-vim.keymap.set("n", "]c", gitsigns.next_hunk)
-vim.keymap.set("n", "[c", gitsigns.prev_hunk)
-
-vim.keymap.set("n", "<leader>ga", gitsigns.stage_hunk)
-vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk)
-vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk)
-vim.keymap.set("n", "<leader>gu", gitsigns.undo_stage_hunk)
-vim.keymap.set("n", "<leader>gq", function()
-    gitsigns.setqflist "all"
-end)
-vim.keymap.set("n", "<leader>g.q", function()
-    gitsigns.setqflist(0)
-end)
-
-vim.keymap.set("o", "ah", gitsigns.select_hunk)
-vim.keymap.set("v", "ah", gitsigns.select_hunk)
