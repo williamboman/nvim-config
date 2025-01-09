07a059b05392725480def74896559f41ccaf0e41
William Boman <william@redwill.se>
2025-01-09 10:00:44 +0100

    wip

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,7 +1,16 @@
 local registry = require("mason-registry")
 
-for _, pkg in ipairs(registry.get_installed_package_names()) do
+local function source_runtime(pkg)
     xpcall(function ()
-        vim.cmd.runtime { ("mason/%s.{lua,vim}"):format(pkg), bang = true }
+        vim.cmd.runtime { ("mason/%s.lua"):format(pkg), bang = true }
+        vim.cmd.runtime { ("mason/%s.vim"):format(pkg), bang = true }
     end, vim.api.nvim_err_writeln)
 end
+
+for _, pkg in ipairs(registry.get_installed_package_names()) do
+    source_runtime(pkg)
+end
+
+registry:on("package:install:success", vim.schedule_wrap(function (pkg)
+    source_runtime(pkg.name)
+end))
